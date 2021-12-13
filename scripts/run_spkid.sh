27c7b372470679218df017bb31580aa4ff353b00
annafalpi <anna.falceto@estudiantat.upc.edu>
2021-12-13 13:30:05 +0100

    Compila i executa 1

--- scripts/run_spkid.sh
+++ scripts/run_spkid.sh
@@ -145,7 +145,7 @@ for cmd in $*; do
        for dir in $db/BLOCK*/SES* ; do
            name=${dir/*\/}
            echo $name ---- 
-           gmm_train -i 0 -v 1 -T 0.001 -N 5 -m 1 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/class/$name.train || exit 1
+           gmm_train -i 0 -v 1 -T 0.001 -N 5 -m 2 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/class/$name.train || exit 1
            echo
        done
    elif [[ $cmd == test ]]; then
